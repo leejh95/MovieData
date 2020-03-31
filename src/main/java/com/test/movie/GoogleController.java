ece561f0d06504359a05192807a9409f2f4d29a0
koitt04 <koitt04@koitt04-PC>
2020-03-31 12:57:17 +0900

    구글로그인

---
+++ src/main/java/com/test/movie/GoogleController.java
@@ -0,0 +1,46 @@
+package com.test.movie;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import mybatis.dao.MovieDAO;
+import mybatis.vo.MovieMemberVO;
+
+@Controller
+public class GoogleController {
+	
+	@Autowired
+	MovieDAO m_dao;
+	
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping("google.inc")
+	public String signin() {
+		
+		return "google";
+	}
+	
+	@RequestMapping("googlelogin.inc")
+	public String googlelogin(MovieMemberVO mvo) {
+		
+		ModelAndView mv = new ModelAndView();
+		
+		MovieMemberVO vo = m_dao.signInNaver(mvo.getSns_id(), mvo.getSns_type());
+		if(vo != null) {
+    		//이미 방문하여 DB에 남아있는경우
+    		session.setAttribute("memVO", vo);
+    	}else {
+    		// 처음온 경우
+    		m_dao.signUp(mvo);
+    		session.setAttribute("memVO", vo);
+    	}
+
+		return "index";
+	}
+	
+}
