629fa922ea39e45fda9e6c26ffbfc50e4014891d
leejh95 <wogns6857@naver.com>
2020-04-01 15:04:00 +0900

    게시판 적용한 db, vo, mapper, dao 수정

--- src/main/java/mybatis/dao/MovieDAO.java
+++ src/main/java/mybatis/dao/MovieDAO.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import mybatis.vo.MovieBoardVO;
 import mybatis.vo.MovieCommentVO;
 import mybatis.vo.MovieMemberVO;
 
@@ -32,16 +33,16 @@ public class MovieDAO {
 		return vo;
 	}
 	
-	// 네이버 로그인
-		public MovieMemberVO signInNaver(String sns_id, String sns_type) {
-			Map<String, String> map = new HashMap<String, String>();
-			map.put("sns_id", sns_id);
-			map.put("sns_type", sns_type);
-			
-			MovieMemberVO vo = ss.selectOne("movie.sign_in_naver", map);
-			
-			return vo;
-		}
+	// SNS 로그인
+	public MovieMemberVO signInSns(String sns_id, String sns_type) {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("sns_id", sns_id);
+		map.put("sns_type", sns_type);
+		
+		MovieMemberVO vo = ss.selectOne("movie.sign_in_sns", map);
+		
+		return vo;
+	}
 	
 	// 회원 가입
 	public boolean signUp(MovieMemberVO vo) {
@@ -148,7 +149,125 @@ public class MovieDAO {
 		
 		return avg;
 	}
-
+	
+	//전체 게시물 수
+	public int totalCount(String category) {
+		int total = 0;
+		
+		total = ss.selectOne("movie.totalCount", category);
+		
+		return total;
+	}
+	
+	//원글 쓰기
+	public boolean writeBoard(MovieBoardVO vo) {
+		boolean chk = false;
+		
+		int cnt = ss.insert("movie.writeBoard", vo);
+		
+		if(cnt > 0)
+			chk = true;
+		
+		return chk;
+	}
+	
+	//답변 쓰기 시 순번 증가
+	public void updateSunbun(Map<String, String> map) {
+		ss.update("movie.updateSunbun", map);
+	}
+	
+	//답변 쓰기
+	public boolean writeAnswer(MovieBoardVO vo) {
+		boolean chk = false;
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("ref", vo.getRef());
+		map.put("sunbun", vo.getSunbun());
+		
+		updateSunbun(map);
+		
+		int sunbun = Integer.parseInt(vo.getSunbun());
+		int step = Integer.parseInt(vo.getStep());
+		
+		vo.setSunbun(String.valueOf(sunbun+1));
+		vo.setStep(String.valueOf(step+1));
+		
+		int cnt = ss.insert("movie.writeAnswer", vo);
+		
+		if(cnt > 0)
+			chk = true;
+		
+		return chk;
+	}
+	
+	//게시물 리스트
+	public MovieBoardVO[] getBoardList(String category, int begin, int end) {
+		MovieBoardVO[] ar = null;
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("begin", String.valueOf(begin));
+		map.put("end", String.valueOf(end));
+		map.put("category", category);
+		
+		List<MovieBoardVO> list = ss.selectList("movie.getBoardList", map);
+		if(!list.isEmpty()) {
+			ar = new MovieBoardVO[list.size()];
+			list.toArray(ar);
+		}
+		
+		return ar;
+	}
+	
+	//게시물 하나보기
+	public MovieBoardVO getBoard(String b_idx) {
+		MovieBoardVO vo = null;
+		vo = ss.selectOne("movie.getBoard", b_idx);
+		return vo;
+	}
+	
+	//게시물 수정
+	public boolean updateBoard(MovieBoardVO vo) {
+		boolean chk = false;
+		
+		int cnt = ss.update("movie.editPost", vo);
+		if(cnt > 0)
+			chk = true;
+		
+		return chk;
+	}
+	
+	//원글 삭제
+	public boolean deleteBoard(String b_idx) {
+		boolean chk = false;
+		
+		int cnt = ss.update("movie.delBoard", b_idx);
+		
+		if(cnt > 0) 
+			chk = true;
+		
+		return chk;
+	}
+	
+	//답글이 달렸는지 알 수 있는 개수 반환
+	public int getRefCount(String ref) {
+		int count = 0;
+		
+		count = ss.selectOne("movie.getRefCount", ref);
+		
+		return count;
+	}
+	
+	//답글이거나 답글이 달린 경우 삭제
+	public boolean deleteBoard2(String b_idx) {
+		boolean chk = false;
+		
+		int cnt = ss.update("movie.delBoard2", b_idx);
+		
+		if(cnt > 0) 
+			chk = true;
+		
+		return chk;
+	}
 }
 
 
